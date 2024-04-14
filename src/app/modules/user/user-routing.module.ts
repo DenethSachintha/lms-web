f251e17821f9cd17e59c01b77fa86de8e2d196c3
Sachin <kbdsjayathilaka@gmail.com>
2024-04-15 01:48:47 +0530

    routes #2

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {UserHomeComponent} from "./components/user-home/user-home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/user/process/home', pathMatch: 'full'},
+  {
+    path: 'process', loadComponent: () => import('./components/user-context/user-context.component')
+      .then(c => c.UserContextComponent), children: [
+      {path: 'home', component: UserHomeComponent},
+
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UserRoutingModule { }
+export class UserRoutingModule {
+}
