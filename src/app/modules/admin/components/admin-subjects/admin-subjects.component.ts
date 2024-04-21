3b75a0a9bf5203baa4274ffba0d3760e34838c17
Sachin <kbdsjayathilaka@gmail.com>
2024-04-21 19:35:41 +0530

    Admin interfaces #2

--- src/app/modules/admin/components/admin-subjects/admin-subjects.component.ts
+++ src/app/modules/admin/components/admin-subjects/admin-subjects.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MatButton} from "@angular/material/button";
+import {MatDialog} from "@angular/material/dialog";
+import {NewSubjectComponent} from "./inner-items/new-subject/new-subject.component";
 
 @Component({
   selector: 'app-admin-subjects',
@@ -12,4 +14,20 @@ import {MatButton} from "@angular/material/button";
 })
 export class AdminSubjectsComponent {
 
+  constructor(private matDialog: MatDialog) {
+  }
+
+  openNewSubjectForm() {
+    console.log('data')
+    let modalData = this.matDialog.open(NewSubjectComponent, {
+      width: '500px',
+      disableClose:true
+    });
+
+    modalData.afterClosed().subscribe(response => {
+      if (response) {
+        //load All subjects here
+      }
+    })
+  }
 }
